60944d99b313c85b74a5f0db53aa829ac2e9b3ba
Loc <loc@192.168.2.3>
2023-12-02 13:44:52 +0700

    edit

--- integration-options/vertex-ai-sdk/src/main/java/com/campbelltech/Main.java
+++ integration-options/vertex-ai-sdk/src/main/java/com/campbelltech/Main.java
@@ -21,26 +21,36 @@ public class Main {
     private final static String LOCATION_ID = System.getenv("LOCATION_ID");
 
     public static void main(String[] args) throws IOException {
-        try (PredictionServiceClient predictionServiceClient = PredictionServiceClient.create(getPredictionServiceSettings())) {
-            final EndpointName endpointName =  EndpointName.ofProjectLocationPublisherModelName(
-                    System.getenv("PROJECT_ID"), LOCATION_ID,
-                    System.getenv("PUBLISHER"),
-                    System.getenv("MODEL_ID"));
-            PredictResponse predictResponse = predictionServiceClient.predict(
-                    endpointName, getInstances(), getParameterValue());
-            System.out.println("Response ->\n" + predictResponse);
-        }
-        main(args);
+
+        PredictionServiceClient predictionServiceClient = PredictionServiceClient.create(getPredictionServiceSettings());
+
+        final EndpointName endpointName = EndpointName.ofProjectLocationPublisherModelName(
+                System.getenv("PROJECT_ID"),
+                LOCATION_ID,
+                System.getenv("PUBLISHER"),
+                System.getenv("MODEL_ID"));
+
+        PredictResponse predictResponse = predictionServiceClient.predict(
+                endpointName, getInstances(), getParameterValue());
+
+        System.out.println("Response ->\n" + predictResponse);
+
+//        main(args);
     }
 
     private static String getPrompt() {
+
         System.out.println("Type a prompt:");
+
         Scanner in = new Scanner(System.in);
+
         return in.nextLine();
     }
 
     private static PredictionServiceSettings getPredictionServiceSettings() throws IOException {
+
         String endpoint = String.format("%s-aiplatform.googleapis.com:443", LOCATION_ID);
+
         return PredictionServiceSettings.newBuilder()
                 .setEndpoint(endpoint)
                 .setCredentialsProvider(FixedCredentialsProvider.create(getGoogleCredentials()))
@@ -48,20 +58,26 @@ public class Main {
     }
 
     private static GoogleCredentials getGoogleCredentials() throws IOException {
+
         return GoogleCredentials
-                .fromStream(Main.class.getClassLoader().getResourceAsStream(
-                        System.getenv("SERVICE_ACCOUNT_FILE_PATH"))
+                .fromStream(Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream(
+                        System.getenv("SERVICE_ACCOUNT_FILE_PATH")))
                 )
-                .createScoped(Lists.newArrayList(System.getenv("SCOPES")));
+                .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
     }
 
     public static String getContentValue() throws IOException {
+
         String context = System.getenv("CONTEXT");
+
         try (InputStreamReader reader = new InputStreamReader(
-                     Main.class.getClassLoader().getResourceAsStream("examples.json"))) {
+
+                Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream("examples.json")))) {
+
             Gson gson = new Gson();
             Map<String, Object>[] examples = gson.fromJson(reader, TypeToken.of(Map[].class).getType());
             StringBuilder inputOutputInstances = new StringBuilder();
+
             for (Map<String, Object> example : examples) {
                 String inputValue = (String) example.get("input");
                 String outputValue = (String) example.get("output");
@@ -72,23 +88,38 @@ public class Main {
     }
 
     private static List<Value> getInstances() throws IOException {
+
         Value.Builder instanceValue = Value.newBuilder();
+
         String instance = String.format("{ \"content\": \"%s\" }", getContentValue());
         JsonFormat.parser().merge(instance, instanceValue);
+
         List<Value> instances = new ArrayList<>();
         instances.add(instanceValue.build());
+
         return instances;
     }
 
     private static Value getParameterValue() throws InvalidProtocolBufferException {
+
         Value.Builder parameterValueBuilder = Value.newBuilder();
+
         Map<String, Object> parameterMap = new HashMap<>();
-        parameterMap.put("candidateCount", Integer.parseInt(System.getenv("CANDIDATE_COUNT")));
-        parameterMap.put("maxOutputTokens", Integer.parseInt(System.getenv("MAX_OUTPUT_TOKENS")));
-        parameterMap.put("temperature", Double.parseDouble(System.getenv("TEMPERATURE")));
-        parameterMap.put("topP", Double.parseDouble(System.getenv("TOP-P")));
-        parameterMap.put("topK", Integer.parseInt(System.getenv("TOP-K")));
+
+//        parameterMap.put("candidateCount", Integer.parseInt(System.getenv("CANDIDATE_COUNT")));
+//        parameterMap.put("maxOutputTokens", Integer.parseInt(System.getenv("MAX_OUTPUT_TOKENS")));
+//        parameterMap.put("temperature", Double.parseDouble(System.getenv("TEMPERATURE")));
+//        parameterMap.put("topP", Double.parseDouble(System.getenv("TOP-P")));
+//        parameterMap.put("topK", Integer.parseInt(System.getenv("TOP-K")));
+
+        parameterMap.put("candidateCount", 1); //TODO: ???
+        parameterMap.put("maxOutputTokens", 1024);
+        parameterMap.put("temperature", 0.2);  //TODO: ???
+        parameterMap.put("topP", 0.8);  //TODO: ???
+        parameterMap.put("topK", 40);  //TODO: ???
+
         JsonFormat.parser().merge(new Gson().toJson(parameterMap), parameterValueBuilder);
+
         return parameterValueBuilder.build();
     }
 }
