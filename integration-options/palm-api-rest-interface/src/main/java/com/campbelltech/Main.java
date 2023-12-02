64833f37d298405f25b44f12a66256daf5c1daf9
Loc.MSE <loc23mse43006@fsb.edu.vn>
2023-12-02 21:33:55 +0700

    edited

--- integration-options/palm-api-rest-interface/src/main/java/com/campbelltech/Main.java
+++ integration-options/palm-api-rest-interface/src/main/java/com/campbelltech/Main.java
@@ -15,47 +15,54 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+
         // NB! In JDK 21 the HttpClient now also implements AutoCloseable and can now be used in try-with-resources block
-        try (HttpClient client = HttpClient.newBuilder().build()) {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(String.format(
-                            "https://generativelanguage.googleapis.com/v1beta3/models/%s:generateMessage?key=%s",
-                            System.getenv("MODEL_ID"), System.getenv("API_KEY")
-                    )))
-                    .header("Content-Type", "application/json")
-                    .POST(HttpRequest.BodyPublishers.ofString(getJsonRequestBody()))
-                    .build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("Response ->\n" + response.body());
-        } catch (URISyntaxException | IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-        // recursively call main to ask for user prompt again
-        main(args);
+        HttpClient client = HttpClient.newBuilder().build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(String.format(
+                        "https://generativelanguage.googleapis.com/v1beta3/models/%s:generateMessage?key=%s",
+                        System.getenv("MODEL_ID"), System.getenv("API_KEY")
+                )))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(getJsonRequestBody()))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Response ->\n" + response.body());
     }
+    // recursively call main to ask for user prompt again
+//        main(args);
 
     private static String getJsonRequestBody() throws IOException, URISyntaxException {
+
         // US locale ensures that doubles contains dots (.) and not commas (,)
         Locale.setDefault(Locale.US);
+
         return String.format("""
-                {
-                    "prompt": {
-                        "context": "%s",
-                        "examples": %s,
-                        "messages": [{ "content": "%s"} ],
-                    },
-                    "temperature": %.1f,
-                    "top_k": %d,
-                    "top_p": %.1f,
-                    "candidate_count": %d
-                }
-                """,
+                        {
+                            "prompt": {
+                                "context": "%s",
+                                "examples": %s,
+                                "messages": [{ "content": "%s"} ],
+                            },
+                            "temperature": %.1f,
+                            "top_k": %d,
+                            "top_p": %.1f,
+                            "candidate_count": %d
+                        }
+                        """,
                 System.getenv("CONTEXT"), getExamples(), getPrompt(),
-                Double.parseDouble(System.getenv("TEMPERATURE")),
-                Integer.parseInt(System.getenv("TOP-K")),
-                Double.parseDouble(System.getenv("TOP-P")),
-                Integer.parseInt(System.getenv("CANDIDATE_COUNT")));
+//                Double.parseDouble(System.getenv("TEMPERATURE")),
+//                Integer.parseInt(System.getenv("TOP-K")),
+//                Double.parseDouble(System.getenv("TOP-P")),
+//                Integer.parseInt(System.getenv("CANDIDATE_COUNT")));
+
+                 Double.parseDouble("0.2"),
+                Integer.parseInt("40"),
+                Double.parseDouble("0.8"),
+                Integer.parseInt("1"));
     }
 
     private static String getPrompt() {
